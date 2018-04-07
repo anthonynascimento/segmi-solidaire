c273afa2aad64d972a2b203cad6d015d97a7cec5
vijay937 <vijay93700@gmail.com>
2018-04-07 19:35:52 +0200

    inscription js edited

--- segmisolidaire.sql
+++ segmisolidaire.sql
@@ -1,25 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- version 4.2.12deb2+deb8u2
+-- http://www.phpmyadmin.net
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  Dim 01 avr. 2018 à 18:00
--- Version du serveur :  5.7.19
--- Version de PHP :  7.0.23
+-- Client :  localhost
+-- Généré le :  Sam 07 Avril 2018 à 16:58
+-- Version du serveur :  5.6.30-1~bpo8+1
+-- Version de PHP :  5.6.33-0+deb8u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Base de données :  `segmisolidaire`
+-- Base de données :  `segmi_rgwk`
 --
 
 -- --------------------------------------------------------
@@ -28,21 +26,18 @@ SET time_zone = "+00:00";
 -- Structure de la table `annale`
 --
 
-DROP TABLE IF EXISTS `annale`;
 CREATE TABLE IF NOT EXISTS `annale` (
-  `idAnnale` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+`idAnnale` bigint(20) unsigned NOT NULL,
   `nom` text NOT NULL,
   `datePublication` int(4) NOT NULL,
   `fichier` text NOT NULL,
   `niveau` varchar(2) NOT NULL,
   `matiere` varchar(64) NOT NULL,
-  `username` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`idAnnale`),
-  UNIQUE KEY `idAnnale` (`idAnnale`)
+  `username` varchar(50) DEFAULT NULL
 ) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `annale`
+-- Contenu de la table `annale`
 --
 
 INSERT INTO `annale` (`idAnnale`, `nom`, `datePublication`, `fichier`, `niveau`, `matiere`, `username`) VALUES
@@ -61,20 +56,17 @@ INSERT INTO `annale` (`idAnnale`, `nom`, `datePublication`, `fichier`, `niveau`,
 -- Structure de la table `cours`
 --
 
-DROP TABLE IF EXISTS `cours`;
 CREATE TABLE IF NOT EXISTS `cours` (
-  `idCours` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+`idCours` bigint(20) unsigned NOT NULL,
   `nomCours` varchar(64) NOT NULL,
   `description` text NOT NULL,
   `niveau` varchar(2) NOT NULL,
   `matiere` varchar(100) NOT NULL,
-  `username` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`idCours`),
-  UNIQUE KEY `idCours` (`idCours`)
+  `username` varchar(50) DEFAULT NULL
 ) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `cours`
+-- Contenu de la table `cours`
 --
 
 INSERT INTO `cours` (`idCours`, `nomCours`, `description`, `niveau`, `matiere`, `username`) VALUES
@@ -104,21 +96,18 @@ INSERT INTO `cours` (`idCours`, `nomCours`, `description`, `niveau`, `matiere`, 
 -- Structure de la table `etudiant`
 --
 
-DROP TABLE IF EXISTS `etudiant`;
 CREATE TABLE IF NOT EXISTS `etudiant` (
-  `idEtudiant` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+`idEtudiant` bigint(20) unsigned NOT NULL,
   `nom` varchar(64) NOT NULL,
   `prenom` varchar(64) NOT NULL,
   `username` text NOT NULL,
   `telephone` int(10) NOT NULL,
   `mdp` text NOT NULL,
-  `roles` varchar(255) CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`idEtudiant`),
-  UNIQUE KEY `idEtudiant` (`idEtudiant`)
+  `roles` varchar(255) CHARACTER SET utf8 NOT NULL
 ) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `etudiant`
+-- Contenu de la table `etudiant`
 --
 
 INSERT INTO `etudiant` (`idEtudiant`, `nom`, `prenom`, `username`, `telephone`, `mdp`, `roles`) VALUES
@@ -135,9 +124,8 @@ INSERT INTO `etudiant` (`idEtudiant`, `nom`, `prenom`, `username`, `telephone`, 
 -- Structure de la table `evenement`
 --
 
-DROP TABLE IF EXISTS `evenement`;
 CREATE TABLE IF NOT EXISTS `evenement` (
-  `idEvenement` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+`idEvenement` bigint(20) unsigned NOT NULL,
   `titre` varchar(100) NOT NULL,
   `description` text NOT NULL,
   `lieu` text NOT NULL,
@@ -145,23 +133,21 @@ CREATE TABLE IF NOT EXISTS `evenement` (
   `dateFin` date NOT NULL,
   `type` varchar(64) NOT NULL,
   `image` text NOT NULL,
-  `username` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`idEvenement`),
-  UNIQUE KEY `idEvenement` (`idEvenement`)
+  `username` varchar(50) DEFAULT NULL
 ) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `evenement`
+-- Contenu de la table `evenement`
 --
 
 INSERT INTO `evenement` (`idEvenement`, `titre`, `description`, `lieu`, `dateDebut`, `dateFin`, `type`, `image`, `username`) VALUES
 (1, 'Safari dans le desertt', '<p>Dubai Le safari dans le d&eacute;sert est une activit&eacute; incontournable &agrave; Duba&iuml;. Vous allez vivre une exp&eacute;rience palpitante dans les dunes de sable du d&eacute;sert d&rsquo;Arabie. Profitez d&rsquo;un excellent d&icirc;ner-barbecue en admirant un spectacle de danse orientale dans un campement situ&eacute; au c&oelig;ur du d&eacute;sert, juste en dehors de la ville moderne de Duba&iuml;.</p>\r\n', ' gfgfggfgfgdfg', '2019-01-01', '2019-01-02', 'soirée', 'even1.jpg', 'mathis@gmail.com'),
 (2, 'Match PSG-REAL', 'Le meilleur match attendu', '', '2018-02-28', '2018-02-28', 'sport', 'foot.jpg', 'mathis@gmail.com'),
-(3, 'Conférence du PDG d\'Apple', 'Presentation de l\'iphone X', '', '2018-02-28', '2018-03-22', 'conférence', 'conf.jpg', NULL),
-(6, 'Trump sa conférence ', 'Le président américain Donald Trump s\'est vanté d\'avoir affirmé au premier ministre canadien Justin Trudeau que Washington affichait un déficit commercial avec Ottawa alors qu\'il n\'en avait «aucune idée», a rapporté jeudi le Washington Post.\r\n\r\nS\'exprimant dans une soirée de collecte de fonds mercredi soir dans le Missouri, M. Trump a raconté cette anecdote, dont le journal dit avoir obtenu un enregistrement audio.\r\n\r\n«Trudeau est venu me voir. C\'est un bon gars, Justin. Il a dit \"non, non, nous n\'avons pas de déficit commercial avec vous, nous n\'en avons aucun\"», a raconté le président en imitant le premier ministre canadien, selon la retranscription du Washington Post.\r\n\r\n«J\'ai dit \"Faux, Justin, vous en avez un\". Je ne savais même pas... Je n\'en avais aucune idée. J\'ai simplement dit \"vous avez tort\"», a poursuivi M. Trump. «Vous savez pourquoi? Parce que nous sommes tellement stupides... Et je pensais qu\'ils étaient malins».\r\n\r\n«J\'ai dit \"eh bien dans ce cas, mon sentiment est différent (...) mais je n\'y crois pas\"», a ajouté le président, affirmant avoir alors «envoyé un de nos gars, son gars, mon gars, ils sont sortis et j\'ai dit \"vérifiez parce que je n\'arrive pas à y croire\"».\r\n\r\nIl est revenu sur le sujet jeudi matin en tweetant: «Nous avons bien un déficit commercial avec le Canada comme nous en avons avec presque tous les pays (certains sont énormes)».', '', '2018-03-28', '2018-03-30', 'conférence', 'even4.jpg', NULL),
+(3, 'Conférence du PDG d''Apple', 'Presentation de l''iphone X', '', '2018-02-28', '2018-03-22', 'conférence', 'conf.jpg', NULL),
+(6, 'Trump sa conférence ', 'Le président américain Donald Trump s''est vanté d''avoir affirmé au premier ministre canadien Justin Trudeau que Washington affichait un déficit commercial avec Ottawa alors qu''il n''en avait «aucune idée», a rapporté jeudi le Washington Post.\r\n\r\nS''exprimant dans une soirée de collecte de fonds mercredi soir dans le Missouri, M. Trump a raconté cette anecdote, dont le journal dit avoir obtenu un enregistrement audio.\r\n\r\n«Trudeau est venu me voir. C''est un bon gars, Justin. Il a dit "non, non, nous n''avons pas de déficit commercial avec vous, nous n''en avons aucun"», a raconté le président en imitant le premier ministre canadien, selon la retranscription du Washington Post.\r\n\r\n«J''ai dit "Faux, Justin, vous en avez un". Je ne savais même pas... Je n''en avais aucune idée. J''ai simplement dit "vous avez tort"», a poursuivi M. Trump. «Vous savez pourquoi? Parce que nous sommes tellement stupides... Et je pensais qu''ils étaient malins».\r\n\r\n«J''ai dit "eh bien dans ce cas, mon sentiment est différent (...) mais je n''y crois pas"», a ajouté le président, affirmant avoir alors «envoyé un de nos gars, son gars, mon gars, ils sont sortis et j''ai dit "vérifiez parce que je n''arrive pas à y croire"».\r\n\r\nIl est revenu sur le sujet jeudi matin en tweetant: «Nous avons bien un déficit commercial avec le Canada comme nous en avons avec presque tous les pays (certains sont énormes)».', '', '2018-03-28', '2018-03-30', 'conférence', 'even4.jpg', NULL),
 (5, 'Gros zbeul sa grand mere', 'On va tout niké tkt meme pas ', '', '2018-03-20', '2018-03-30', 'soirée', 'even1.jpg', NULL),
-(7, 'Soirée de fou malade', 'Et de poursuivre: «PM Justin Trudeau du Canada, un gars très bien, n\'aime pas dire que le Canada a un surplus face aux USA (en négociation), mais c\'est le cas... c\'est le cas pour presque tous... et c\'est comme ça que je sais!»\r\n\r\nD\'après les statistiques américaines, les États-Unis présentaient un excédent commercial (biens et services) avec le Canada de 12,5 milliards de dollars en 2016, pour 627,8 milliards de dollars d\'échanges. Dans le détail, ils affichaient un déficit de 12,1 milliards dans les biens et un excédent de 24,6 milliards dans les services.', '', '2018-03-22', '2018-03-31', 'soirée', 'even3.jpg', NULL),
-(8, 'Choc des héros L\'angleterre ', '«Le Canada et les États-Unis ont une relation commerciale équilibrée et mutuellement bénéfique», a-t-il insisté auprès de l\'AFP.\r\n\r\nDu reste, le cabinet de Justin Trudeau a refusé de commenter les propos que M. Trump auraient tenus mercredi soir.', '', '2018-03-16', '2018-04-20', 'sport', 'even5.jpg', NULL),
+(7, 'Soirée de fou malade', 'Et de poursuivre: «PM Justin Trudeau du Canada, un gars très bien, n''aime pas dire que le Canada a un surplus face aux USA (en négociation), mais c''est le cas... c''est le cas pour presque tous... et c''est comme ça que je sais!»\r\n\r\nD''après les statistiques américaines, les États-Unis présentaient un excédent commercial (biens et services) avec le Canada de 12,5 milliards de dollars en 2016, pour 627,8 milliards de dollars d''échanges. Dans le détail, ils affichaient un déficit de 12,1 milliards dans les biens et un excédent de 24,6 milliards dans les services.', '', '2018-03-22', '2018-03-31', 'soirée', 'even3.jpg', NULL),
+(8, 'Choc des héros L''angleterre ', '«Le Canada et les États-Unis ont une relation commerciale équilibrée et mutuellement bénéfique», a-t-il insisté auprès de l''AFP.\r\n\r\nDu reste, le cabinet de Justin Trudeau a refusé de commenter les propos que M. Trump auraient tenus mercredi soir.', '', '2018-03-16', '2018-04-20', 'sport', 'even5.jpg', NULL),
 (9, 'Sortie entre poto', '<p><strong>Vous aure pour occasion de :</strong></p>\r\n\r\n<ul>\r\n	<li>Traversez un lac rude</li>\r\n	<li>Avoir de bons amis</li>\r\n</ul>\r\n\r\n<ol>\r\n	<li>De faire du kayak</li>\r\n</ol>\r\n', '', '2008-02-27', '2022-08-28', 'soirée', 'even2.jpg', NULL),
 (14, 'test user', '<p>gfgdfgdgdgf</p>\r\n', 'gfdfgdfgdffgdfg', '2018-01-01', '2018-01-01', 'conférence', '2017_novitec_tesla_model_s_4k_6-1920x1080.jpg', 'mathis@gmail.com');
 
@@ -171,25 +157,22 @@ INSERT INTO `evenement` (`idEvenement`, `titre`, `description`, `lieu`, `dateDeb
 -- Structure de la table `job`
 --
 
-DROP TABLE IF EXISTS `job`;
 CREATE TABLE IF NOT EXISTS `job` (
-  `idJob` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+`idJob` bigint(20) unsigned NOT NULL,
   `titre` text NOT NULL,
   `description` text NOT NULL,
   `categorie` varchar(64) NOT NULL,
-  `username` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`idJob`),
-  UNIQUE KEY `idJob` (`idJob`)
+  `username` varchar(100) DEFAULT NULL
 ) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `job`
+-- Contenu de la table `job`
 --
 
 INSERT INTO `job` (`idJob`, `titre`, `description`, `categorie`, `username`) VALUES
 (1, 'Film Xxxx', '<ul>\r\n	<li><strong>comme never tu vas prendre cher</strong></li>\r\n</ul>\r\n', 'babysitting', 'mathis@gmail.com'),
 (2, 'mlmlm', '<ul>\r\n	<li>lmlmll</li>\r\n</ul>\r\n', 'babysitting', NULL),
-(3, 'Agent d\'acceuil', '<p>En gare ou a&eacute;roport</p>\r\n', 'hotessariat', NULL),
+(3, 'Agent d''acceuil', '<p>En gare ou a&eacute;roport</p>\r\n', 'hotessariat', NULL),
 (4, 'Aide aux devoirs', '<ul>\r\n	<li>test&nbsp;</li>\r\n	<li>test</li>\r\n	<li>test</li>\r\n	<li><em>okkkkkk</em></li>\r\n</ul>\r\n', 'services', NULL),
 (7, 'test user000000', '<p>fdfgsdggdfgdfgdfggfgggffg</p>\r\n', 'babysitting', 'mathis@gmail.com');
 
@@ -199,21 +182,18 @@ INSERT INTO `job` (`idJob`, `titre`, `description`, `categorie`, `username`) VAL
 -- Structure de la table `livre`
 --
 
-DROP TABLE IF EXISTS `livre`;
 CREATE TABLE IF NOT EXISTS `livre` (
-  `idLivre` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+`idLivre` bigint(20) unsigned NOT NULL,
   `titre` varchar(100) NOT NULL,
   `auteur` varchar(100) NOT NULL,
   `prix` decimal(10,2) NOT NULL,
   `niveau` varchar(2) NOT NULL,
   `matiere` varchar(100) NOT NULL,
-  `username` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`idLivre`),
-  UNIQUE KEY `idLivre` (`idLivre`)
+  `username` varchar(50) DEFAULT NULL
 ) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `livre`
+-- Contenu de la table `livre`
 --
 
 INSERT INTO `livre` (`idLivre`, `titre`, `auteur`, `prix`, `niveau`, `matiere`, `username`) VALUES
@@ -222,8 +202,81 @@ INSERT INTO `livre` (`idLivre`, `titre`, `auteur`, `prix`, `niveau`, `matiere`, 
 (5, 'la cuisine ', 'yves cote de porc', '2.50', 'M2', 'M2: OSPS', NULL),
 (7, 'le renard et la fourmie', 'jean de la fontaine', '3.50', 'L2', 'L2 UP3: Droit', NULL),
 (8, 'test user000', 'fdgdfgdfg', '1.40', 'L3', 'L3 UPE: Droit', 'mathis@gmail.com');
-COMMIT;
 
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `annale`
+--
+ALTER TABLE `annale`
+ ADD PRIMARY KEY (`idAnnale`), ADD UNIQUE KEY `idAnnale` (`idAnnale`);
+
+--
+-- Index pour la table `cours`
+--
+ALTER TABLE `cours`
+ ADD PRIMARY KEY (`idCours`), ADD UNIQUE KEY `idCours` (`idCours`);
+
+--
+-- Index pour la table `etudiant`
+--
+ALTER TABLE `etudiant`
+ ADD PRIMARY KEY (`idEtudiant`), ADD UNIQUE KEY `idEtudiant` (`idEtudiant`);
+
+--
+-- Index pour la table `evenement`
+--
+ALTER TABLE `evenement`
+ ADD PRIMARY KEY (`idEvenement`), ADD UNIQUE KEY `idEvenement` (`idEvenement`);
+
+--
+-- Index pour la table `job`
+--
+ALTER TABLE `job`
+ ADD PRIMARY KEY (`idJob`), ADD UNIQUE KEY `idJob` (`idJob`);
+
+--
+-- Index pour la table `livre`
+--
+ALTER TABLE `livre`
+ ADD PRIMARY KEY (`idLivre`), ADD UNIQUE KEY `idLivre` (`idLivre`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `annale`
+--
+ALTER TABLE `annale`
+MODIFY `idAnnale` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
+--
+-- AUTO_INCREMENT pour la table `cours`
+--
+ALTER TABLE `cours`
+MODIFY `idCours` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=25;
+--
+-- AUTO_INCREMENT pour la table `etudiant`
+--
+ALTER TABLE `etudiant`
+MODIFY `idEtudiant` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT pour la table `evenement`
+--
+ALTER TABLE `evenement`
+MODIFY `idEvenement` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
+--
+-- AUTO_INCREMENT pour la table `job`
+--
+ALTER TABLE `job`
+MODIFY `idJob` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT pour la table `livre`
+--
+ALTER TABLE `livre`
+MODIFY `idLivre` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
